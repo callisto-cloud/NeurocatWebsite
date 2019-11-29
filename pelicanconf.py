71b8c8f8f0fce057e382894bbfc2634aa115fb48
Simon Schlotter <simontest2.test@gmail.com>
2019-11-29 18:10:09 +0100

    added jinja2 variables for menu items

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,22 +12,21 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
 
+MENUITEMS = (
+    ('Home', '/index.html'),
+    ('Products', '/pages/service.html'),
+    ('About', '/pages/about.html'),
+    ('News', '/category/neurocat-news.html'),
+    ('Jobs', '/pages/jobs.html'),
+    ('Contact','#')
+)
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
